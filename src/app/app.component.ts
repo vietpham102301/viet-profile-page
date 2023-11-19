36258622723870e41db447354fdbd684584ac2d7
vietpham102301 <vietpham102301@gmail.com>
2023-11-20 01:48:21 +0700

    profile-data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
